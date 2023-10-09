b86a99441eb5185e012f27aa68a429a6ea20d7ff
carozum <czumbiehlfaure@hotmail.fr>
2023-10-09 11:51:01 +0200

    adding the functionality to save the list of note in a file

--- Projet_python_Caroline/Exo9_python.py
+++ Projet_python_Caroline/Exo9_python.py
@@ -1,3 +1,4 @@
+import pickle
 print(
     f"""*****************************
     \nEXERCICE 9 - quelques notes
@@ -5,14 +6,28 @@ print(
     \r*****************************""")
 
 
-def saisir_notes():
-    list_notes = []
+def save_list(list_name, name):
+    pickle_out = open(f"{name}.pickle", "wb")
+    pickle.dump(list_name, pickle_out)
+    pickle_out.close()
+
+
+def open_list(name):
+    pickle_in = open(f"{name}.pickle", "rb")
+    return pickle.load(pickle_in)
+
+
+def display_lists():
+    pass
+
+
+def enter_notes(list_notes=[]):
 
     while True:
         note = input("\nNouvelle note: ")
         if note == '':
             print("See you soon")
-            return
+            break
         else:
             try:
                 note = int(note)
@@ -29,15 +44,48 @@ def saisir_notes():
                 number_of_notes = len(list_notes)
                 average = sum(list_notes) / number_of_notes
                 print(f"""
-                    \nNombre de notes saisies: {number_of_notes}
+                    \nNombre de notes: {number_of_notes}
                     \rMoyenne des notes: {round(average, 1)}
                     \rNote la plus basse: {min(list_notes)}
                     \rNote la plus haute: {max(list_notes)}
                     """)
+    return list_notes
 
 
 def app():
-    saisir_notes()
+    choice = input("""
+                \nBienvenue dans le module de saisie des notes.
+                \rQue voulez vous faire ?
+                \rA - Continuer une liste existante?
+                \rB - Voulez vous créer une nouvelle liste?
+                \rC - Voulez vous écraser une liste existante?
+                \rQ - Voulez vous sortir ?""")
+
+    if choice.upper() == "A":
+        # donner les listes disponibles
+        name = input("What is the name of the list?")
+        list_notes = open_list(name)
+        # print(list_notes)
+        print(f"List {name}:")
+        list_notes = enter_notes(list_notes)
+        # print(list_notes)
+        save_list(list_notes, name)
+        pass
+
+    elif choice.upper() == "B":
+        name = input("What is the name of the new list?")
+        list_notes = enter_notes()
+        print(list_notes)
+        save_list(list_notes, name)
+
+    elif choice.upper() == "C":
+        pass
+    elif choice.upper() == 'Q':
+        print("See you soon")
+        return ''
+
+
+# https://pythonprogramming.net/python-pickle-module-save-objects-serialization/
 
 
 if __name__ == "__main__":
